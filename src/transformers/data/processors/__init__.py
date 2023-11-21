13f8c4c8008fc6d475b6cb7996329d72058bb503
xd2287 <xd2287@columbia.edu>
2023-11-21 15:42:43 -0500

    modify the sliding window generation process for segmentation
    the sliding windows may have less overlapping than the original

--- src/transformers/data/processors/__init__.py
+++ src/transformers/data/processors/__init__.py
@@ -13,6 +13,6 @@
 # limitations under the License.
 
 from .glue import glue_convert_examples_to_features, glue_output_modes, glue_processors, glue_tasks_num_labels
-from .squad import SquadExample, SquadFeatures, SquadV1Processor, SquadV2Processor, squad_convert_examples_to_features
+from .squad import SquadExample, SquadFeatures, SquadV1Processor, SquadV2Processor, squad_convert_examples_to_features, squad_convert_examples_to_features_with_segmentation
 from .utils import DataProcessor, InputExample, InputFeatures, SingleSentenceClassificationProcessor
 from .xnli import xnli_output_modes, xnli_processors, xnli_tasks_num_labels
